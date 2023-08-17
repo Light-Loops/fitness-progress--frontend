64bcaba2d17a2eea2e4d03d2efcb390ffe10f7c9
jfpanchi <jfpanchi2@espe.edu.ec>
2023-08-16 23:19:47 -0500

    Feat(ui): Add Logo
    In this commit, I added logo for ui.

--- src/layouts/Auth/index.tsx
+++ src/layouts/Auth/index.tsx
@@ -16,8 +16,9 @@ export const AuthLayout: React.FC<Auth> = ({title, children}) => {
         justifyContent="center"
         sx={{ minHeight: '100vh' }}
       >
-        <Grid item>
-          <Paper sx={{ padding: '1.2em', borderRadius: '0.5em',backgroundColor:'#061A26' }}>
+        <Grid item sx={{position: 'relative'}}>
+          <Paper sx={{ padding: '1.2em', borderRadius: '0.5em',backgroundColor:'#061A26',mt:'48px', mb:"8px"} }>
+          <img src="./fitness-progress.svg" alt='fitness-progress' width='80px' height='80px' style={{position:"absolute", right:"-10px", top:"8px"}}/>
             <Typography sx={{ mt: 1, mb: 1 }} variant="h5">
               {title}
             </Typography>
